64862f2c5dee7100a051c7cc4b510749d5588d17
Álvaro Peña <alvaropg@gmail.com>
2013-04-07 17:25:41 +0200

    Changed module name

--- config/application.config.php
+++ config/application.config.php
@@ -2,7 +2,7 @@
 return array(
     'modules' => array(
         'Application',
-        'Api',
+        'Main',
     ),
     'module_listener_options' => array(
         'config_glob_paths'    => array(
